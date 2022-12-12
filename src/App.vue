e92d8544ed314e6d1d5d8d2f71dc7e7c678500f5
sun <2987488913@qq.com>
2022-12-12 11:53:14 +0800

    首页底部构建

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/cart">Cart</router-link>
-    <router-link to="/order">Order</router-link>
-    <router-link to="/my">My</router-link>
-  </nav>
   <router-view/>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App'
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    home
+  <div class="header">
+    <Docker />
   </div>
 </template>
 <script>
+import Docker from './Docker.vue'
+export default {
+  name: 'Home',
+  components: { Docker }
+}
 </script>
+<style lang="scss" scoped>
+.header {
+  overflow-y: auto;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  padding: 0 .18rem;
+}
+</style>
