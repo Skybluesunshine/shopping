60cbb0bff15d7c980f8e3f7fc7e7e12f8f1059de
sun <2987488913@qq.com>
2022-12-16 15:28:50 +0800

    订单页面弹框布局

--- src/views/order/BottomArea.vue
+++ src/views/order/BottomArea.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="order">
-      <div class="order__price">实付金额 <b>&yen;100</b></div>
-      <div class="order__btn">提交订单</div>
+    <div class="order__price">实付金额 <b>&yen;100</b></div>
+    <div class="order__btn">提交订单</div>
+  </div>
+  <div class="mask">
+    <div class="mask__content">
+      <h3 class="mask__content__title">确认要离开收银台？</h3>
+      <p class="mask__content__desc">请尽快完成支付，否则将被取消</p>
+      <div class="mask__content__btns">
+        <div
+          class="mask__content__btn mask__content__btn--first"
+          @click="handleCancelOrder"
+        >取消订单</div>
+        <div
+          class="mask__content__btn mask__content__btn--last"
+          @click="handleConfirmOrder"
+        >确认支付</div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
