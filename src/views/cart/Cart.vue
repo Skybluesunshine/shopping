9f209b7815851e1ff0fc885c828049aabd0ff2ce
sun <2987488913@qq.com>
2022-12-19 17:17:50 +0800

    Cart

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    cart
+  <div class="wrapper">
+    <Docker :currentIndex="1"/>
   </div>
 </template>
 <script>
+import Docker from '../../components/Docker.vue'
+export default {
+  name: 'Cart',
+  components: { Docker }
+}
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  overflow-y: auto;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  background: rgb(248, 248,248);
+}
+</style>
